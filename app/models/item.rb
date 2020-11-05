00856240c179dba4f2b297e904c169aebd67d2f5
ysk178 <kk.ss.01tt.ht1985@gmail.com>
2020-11-05 14:24:40 +0900

    商品登録

---
+++ app/models/item.rb
@@ -0,0 +1,29 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :charge
+  belongs_to :prefecture
+  belongs_to :day
+  
+  belongs_to :user
+  has_one :record
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :category_id
+    validates :status_id
+    validates :charge_id
+    validates :prefecture_id
+    validates :days_id
+    validates :price
+  end
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :charge_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :days_id, numericality: { other_than: 1 }
+end
