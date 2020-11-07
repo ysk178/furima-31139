3231c9db4ca51860b97cb9dfe90b80db13494896
ysk178 <kk.ss.01tt.ht1985@gmail.com>
2020-11-07 13:19:37 +0900

    商品詳細表示機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(
-      :name, :explanation, :category_id, :status_id, :charge_id, :prefecture_id, :days_id, :price, :image
+      :name, :explanation, :category_id, :status_id, :charge_id, :prefecture_id, :day_id, :price, :image
     ).merge(user_id: current_user.id)
   end
 end
