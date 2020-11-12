48bdcd9a3fff660838f3f508451a5f43ccee2a24
ysk178 <kk.ss.01tt.ht1985@gmail.com>
2020-11-12 20:18:49 +0900

    リダイレクト処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,10 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
   def index
     @item = Item.find(params[:item_id])
-    if current_user.id == @item.user.id
-      redirect_to root_path
-    end
-    if @item.order
+    if current_user.id == @item.user.id || @item.order
       redirect_to root_path
     end
     @order_address = OrderAddress.new
