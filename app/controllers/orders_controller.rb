6af38df43e7ba27fea31724978e81b2beba139ec
ysk178 <kk.ss.01tt.ht1985@gmail.com>
2020-11-13 11:30:45 +0900

    購入機能実装の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -28,7 +28,7 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = "sk_test_b347117a08ac2e59c6991567"
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
       card: order_params[:token],
